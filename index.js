e2e69fee4f22e7c9334a0feeb64deaddaf18e5a4
eatmore314 <eatmore314@yahoo.com>
2024-12-23 14:05:09 -0500

    new update

--- index.js
+++ index.js
@@ -1,2 +1,57 @@
+let play = document.getElementById("playButton");
+let next = document.getElementById("nextButton");
+let reset = document.getElementById("resetButton");
+let pause = document.getElementById("pauseButton");
+let myAudio = document.getElementById('myAudio');
+let myArtist = document.getElementById("myArtist");
+let myTitle = document.getElementById("myTitle");
+let myImage = document.getElementById("myImage");
+let currentIndex = 0;
+
+let playlist = [{
+    song: "./music/sweet.mp3",
+    artist: "Rheyr",
+    title: "Sweet",
+    image: "./images/sweet.png"
+},
+{
+    song: "./music/kurse.mp3",
+    artist: "Axgxst",
+    title: "Kurse",
+    image: "./images/kurse.png"
+},
+{
+    song: "./music/bruh.mp3",
+    artist: "bruhmanegodr",
+    title: "?",
+    image: "./images/bruhmanegod.png"
+}]
 
 
+
+play.addEventListener("click", ()=> {
+    myAudio.play()
+})
+
+function updateTrack() {
+    const track = playlist[currentIndex];
+    myArtist.textContent = track.artist;
+    myTitle.textContent = track.title;
+    myImage.src = track.image;
+    myAudio.src = track.song;
+    myAudio.load();
+    myAudio.play();
+}
+
+next.addEventListener("click", ()=> {
+    currentIndex = (currentIndex + 1) % playlist.length;
+    updateTrack()
+})
+
+updateTrack()
+
+
+
+play.addEventListener("click", ()=> {
+    myAudio.play()
+})
